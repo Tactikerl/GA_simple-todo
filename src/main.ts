2f72b994adf57ba5fc34c60c5a89fecafccef2ae
eskjelbred <eskjelbred@gmail.com>
2026-04-13 15:17:55 +0200

    lagt til loginfunksjon og type for response

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { login } from "./api";
+
+const loginSection = document.getElementById('login-section') as HTMLElement;
+const loginForm = document.getElementById('login-form') as HTMLFormElement;
+const loginError = document.getElementById('login-error') as HTMLParagraphElement;
